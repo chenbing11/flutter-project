e1c2ba0a115e9f14e88fada18f645765885ae8b4
chenbing1 <cb@MacBook-Pro-2.local>
2021-12-03 23:31:23 +0800

    #00000

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_custom_dialog/FlutterCustomDialogPlugin.h>)
+#import <flutter_custom_dialog/FlutterCustomDialogPlugin.h>
+#else
+@import flutter_custom_dialog;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterCustomDialogPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterCustomDialogPlugin"]];
+}
+
+@end
